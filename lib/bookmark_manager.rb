b078f8aedae319c604b6be7cd7e77bafce1977e5
Jade King <dev@jadeking.co.uk>
2015-04-07 12:35:02 +0100

    update

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,10 +1,10 @@
 require 'sinatra/base'
-
-class bookmark_manager < Sinatra::Base
+# hkjhkh
+class BookmarkManager < Sinatra::Base
   get '/' do
     'Hello bookmark_manager!'
   end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
